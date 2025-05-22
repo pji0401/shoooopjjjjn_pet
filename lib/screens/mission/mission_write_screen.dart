c59daece0c1bf00f8eef39a3d57b54d74ff30297
taeyoung24 <jtyoung24@naver.com>
2025-05-22 22:40:24 +0900

    Refactor image handling by creating ImageAttachingSection and updating MissionWriteScreen to use it

--- lib/screens/mission/mission_write_screen.dart
+++ lib/screens/mission/mission_write_screen.dart
@@ -3,16 +3,10 @@ import 'package:flutter_svg/svg.dart';
 import 'package:pawprints/config/index.dart';
 import 'package:pawprints/config/route/route_path.dart';
 import 'package:pawprints/config/theme/colors.dart';
+import 'package:pawprints/services/app_logger.dart';
 import 'package:pawprints/widgets/base_scaffold.dart';
 import 'package:pawprints/widgets/elevated_button.dart';
-
-// class that holds image data
-class CardItem {
-  final String id;
-  final String imageUrl;
-
-  CardItem({required this.imageUrl}) : id = UniqueKey().toString();
-}
+import 'package:pawprints/widgets/sections.dart';
 
 class MissionWriteScreen extends StatefulWidget {
   const MissionWriteScreen({super.key});
@@ -25,33 +19,9 @@ class _MissionWriteScreenState extends State<MissionWriteScreen> {
   final TextEditingController _textController = TextEditingController();
   String _selectedDateString = "2025.04.17";
 
-  // Sampe list of images
-  final List<CardItem> _imageItems = [
-    CardItem(
-        imageUrl:
-            'https://images.unsplash.com/photo-1541451563641-c2bdc370f58a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
-    CardItem(
-        imageUrl:
-            'https://images.unsplash.com/photo-1505628346881-b72b27e84530?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNhdCUyMGZsb3dlcnxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60'),
-  ];
-
-  final int maxImages = 10;
-
-  void _addImage() {
-    if (_imageItems.length < 10) {
-      setState(() {
-        _imageItems.add(CardItem(
-            imageUrl:
-                'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZnVubnklMjBkb2d8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'));
-      });
-    }
-  }
-
-  void _removeImage(String id) {
-    setState(() {
-      _imageItems.removeWhere((item) => item.id == id);
-    });
-  }
+// ImageAttachingSection의 상태에 접근하기 위한 GlobalKey
+  final GlobalKey<ImageAttachingSectionState> _imageSectionKey =
+      GlobalKey<ImageAttachingSectionState>();
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
@@ -74,115 +44,8 @@ class _MissionWriteScreenState extends State<MissionWriteScreen> {
     super.dispose();
   }
 
-  // 이미지 갤러리 섹션을 별도의 메소드로 분리
-  Widget _buildImageGallerySection(double imageHeight, double imageWidth) {
-    return SizedBox(
-      height: imageHeight + 30,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // --- Add Button ---
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              GestureDetector(
-                onTap: _addImage,
-                child: Container(
-                  width: 60,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: AppColors.main,
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                  child: const Icon(Icons.add, color: Colors.white, size: 35),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                "${_imageItems.length}/$maxImages",
-                style: const TextStyle(
-                  color: AppColors.main,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(width: 16),
-          // --- Horizontally Scrollable Images ---
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: _imageItems.length,
-              itemBuilder: (context, index) {
-                final item = _imageItems[index];
-                return Padding(
-                  padding: const EdgeInsets.only(right: 12.0),
-                  child: Stack(
-                    children: [
-                      Container(
-                        width: imageWidth,
-                        height: imageHeight,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(18),
-                          border:
-                              Border.all(color: Colors.grey.shade200, width: 4),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(
-                            item.imageUrl,
-                            fit: BoxFit.fill,
-                            loadingBuilder: (context, child, progress) {
-                              if (progress == null) return child;
-                              return const Center(
-                                  child: CircularProgressIndicator());
-                            },
-                            errorBuilder: (context, error, stackTrace) {
-                              return Container(
-                                color: Colors.grey.shade200,
-                                child: const Icon(Icons.broken_image,
-                                    color: Colors.grey),
-                              );
-                            },
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 8,
-                        right: 8,
-                        child: GestureDetector(
-                          onTap: () => _removeImage(item.id),
-                          child: Container(
-                            padding: const EdgeInsets.all(4),
-                            decoration: BoxDecoration(
-                              color: Colors.white.withOpacity(
-                                  0.6), // withValues 대신 withOpacity 사용
-                              shape: BoxShape.circle,
-                            ),
-                            child: const Icon(Icons.close,
-                                color: AppColors.black, size: 14),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    // dynamic height for images, [screen height / 3.5 or 4]
-    final double imageHeight =
-        MediaQuery.of(context).size.height / 3.4; // Adjust this ratio as needed
-    final double imageWidth = imageHeight * 0.8; // To make it slightly portrait
-
     return BaseScaffold(
       title: '미션 인증',
       leadingItem: IconButton(
@@ -199,7 +62,9 @@ class _MissionWriteScreenState extends State<MissionWriteScreen> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               // --- Image Gallery Section ---
-              _buildImageGallerySection(imageHeight, imageWidth),
+              ImageAttachingSection(
+                key: _imageSectionKey,
+              ),
 
               const SizedBox(height: 24),
 
@@ -257,6 +122,17 @@ class _MissionWriteScreenState extends State<MissionWriteScreen> {
               CustomElevatedButton(
                 text: '인증하기',
                 onPressed: () {
+                  final List<CardItem> attachedImages =
+                      _imageSectionKey.currentState?.currentImages ?? [];
+                  final String missionText = _textController.text;
+                  final String selectedDate = _selectedDateString;
+
+                  // 여기서 attachedImages, missionText, selectedDate를 사용하여
+                  // 서버 전송 또는 다른 필요한 로직을 수행합니다.
+                  AppLogger.i('첨부된 이미지 수: ${attachedImages.length}');
+                  AppLogger.i('입력된 텍스트: $missionText');
+                  AppLogger.i('선택된 날짜: $selectedDate');
+
                   context.push(RoutePath.mission_complete.value);
                 },
                 backgroundColor: const Color(0xFF4A80F0),
